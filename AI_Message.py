575dfc78699a82bc01001f5442e797bb58ed5439
parsha221 <parshasravan.kumar@cognizant.com>
2025-03-06 18:48:47 +0530

    Create AI_Message.py

---
+++ AI_Message.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+from langchain_core.messages import AIMessage, HumanMessage
+
+messages = [AIMessage(content="hi how can i help you?", name="Model")]
+messages.append(HumanMessage(content="add 2 and 5 and then mulitply by 10 and divivde by 7", name="Lance"))
+
+#for m in messages:
+ #   m.pretty_print()
+
+from langchain_ollama import OllamaLLM
+
+llm = OllamaLLM(model="llama3.1:latest")
+
+max_iterations =1
+iteration = 0
+
+while iteration < max_iterations:
+result = llm.invoke(messages)
+if result == messages[-1].content:
+break
+messages.append(AIMessage(content=result, name="Model"))
+iteration += 1
+print(result)
+print(".......................")
+ans=llm.invoke(result)
+print(ans)
